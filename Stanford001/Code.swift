4dd350e5bdda2140d015ff946cf8646b61060777
mobappdeveloper22 <mobappdeveloper22@gmail.com>
2026-03-21 13:50:36 +0300

    L5 Code

---
+++ Stanford001/Code.swift
@@ -0,0 +1,83 @@
+//
+//  Code.swift
+//  Stanford001
+//
+//  Created by ZuhuAhmu on 21/03/2026.
+//
+
+import SwiftUI
+
+extension Peg {
+    static let missing = Color.clear
+}
+
+struct Code {
+    var kind : Kind
+    var pegs : [Peg] = Array(repeating: .missing, count: 4)
+    
+    static let missingPeg : Peg = .missing
+    
+    enum Kind : Equatable {
+        case master
+        case guess
+        case attempt([Match])
+        case uknown
+    }
+    
+    mutating func randomize(from pegChoices : [Peg]) {
+        for index in pegChoices.indices {
+            pegs[index] = pegChoices.randomElement() ?? Code.missingPeg
+        }
+    }
+    
+    var matches:[Match] {
+        switch kind {
+        case .attempt(let matches):
+            return matches
+        default:
+            return []
+        }
+    }
+    
+    func match(against otherCode : Code) -> [Match] {
+        var pegsToMatch = otherCode.pegs
+        
+        var backwardsExactMatches : [Match] = pegs.indices.reversed().map { index in
+            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+                pegsToMatch.remove(at: index)
+                return .exact
+            } else {
+                return .nomatch
+            }
+        }
+        
+        let exactMatches : [Match] = backwardsExactMatches.reversed()
+        return pegs.indices.map { index in
+            if exactMatches[index] != .exact , let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+                pegsToMatch.remove(at: matchIndex)
+                return .inexact
+            } else {
+                return exactMatches[index]
+            }
+        }
+        
+//        // alternate is above
+//        var results : [Match] = Array(repeating: .nomatch, count: pegs.count)
+//        for index in pegs.indices.reversed() {
+//            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
+//                results[index] = .exact
+//                pegsToMatch.remove(at: index)
+//            }
+//        }
+//
+//        for index in pegs.indices {
+//            if exactMatches[index] != .exact {
+//                if let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
+//                    exactMatches[index] = .inexact
+//                    pegsToMatch.remove(at: matchIndex)
+//                }
+//            }
+//        }
+//        return exactMatches
+    }
+}
--- Stanford001/CodeBreaker.swift
+++ Stanford001/CodeBreaker.swift
@@ -27,7 +27,7 @@ struct CodeBreaker {
             let newPage = pegChoices[(indexOfExistingPegInPegChoices+1) % pegChoices.count]
             guess.pegs[index] = newPage
         } else {
-            guess.pegs[index] = pegChoices.first ?? Code.missing
+            guess.pegs[index] = pegChoices.first ?? Code.missingPeg
         }
     }
     
@@ -39,56 +39,5 @@ struct CodeBreaker {
     
 }
 
-struct Code {
-    var kind : Kind
-    var pegs : [Peg] = Array(repeating: Code.missing, count: 4)
-    
-    static let missing : Peg = .clear
-    
-    enum Kind : Equatable {
-        case master
-        case guess
-        case attempt([Match])
-        case uknown
-    }
-    
-    mutating func randomize(from pegChoices : [Peg]) {
-        for index in pegChoices.indices {
-            pegs[index] = pegChoices.randomElement() ?? Code.missing
-        }
-    }
-    
-    var matches:[Match] {
-        switch kind {
-        case .attempt(let matches):
-            return matches
-        default:
-            return []
-        }
-    }
-    
-    func match(against otherCode : Code) -> [Match] {
-        var results : [Match] = Array(repeating: .nomatch, count: pegs.count)
-        var pegsToMatch = otherCode.pegs
-        
-        for index in pegs.indices.reversed() {
-            if pegsToMatch.count > index, pegsToMatch[index] == pegs[index] {
-                results[index] = .exact
-                pegsToMatch.remove(at: index)
-            }
-        }
-        
-        for index in pegs.indices {
-            if results[index] != .exact {
-                if let matchIndex = pegsToMatch.firstIndex(of: pegs[index]) {
-                    results[index] = .inexact
-                    pegsToMatch.remove(at: matchIndex)
-                }
-            }
-        }
-        
-        return results
-    }
-}
 
 
